212a6c8971450aed089a51ea18665077f7840ee8
Eduardo Cruz Matias <ecruzmatias5@gmail.com>
2023-09-10 21:46:59 -0500

    gran avance

--- main.js
+++ main.js
@@ -1,33 +1,72 @@
-let nuevoTexto = "";
+let inputText = document.getElementById("ingresado");
+let btnEncriptar = document.getElementById("encriptar");
+let btnDesencriptar = document.getElementById("desencriptar");
+let outputInicial = document.getElementById("empty_output");
+let outpuMsg = document.getElementById("output_with_msg");
+let copiar = document.getElementById("copiarMsg");
+let output = document.getElementById("output");
 
-function encriptar() {
-  let texto = document.querySelector("#ingresado").value;
-  nuevoTexto = texto.replace(/e/g, "enter");
-  nuevoTexto = nuevoTexto.replace(/i/g, "imes");
-  nuevoTexto = nuevoTexto.replace(/a/g, "ai");
-  nuevoTexto = nuevoTexto.replace(/o/g, "ober");
-  nuevoTexto = nuevoTexto.replace(/u/g, "ufat");
-  document.querySelector("#encriptado").innerHTML = nuevoTexto;
-  document.querySelector("#ingresado").value = "";
-  console.log(nuevoTexto);
-  return nuevoTexto;
+const llaves = {
+  'e': 'enter',
+  'i': 'imes',
+  'a': 'ai',
+  'o': 'ober',
+  'u': 'ufat'
 }
 
-function desencriptar() {
-  let texto = document.querySelector("#ingresado").value;
+//revierte la posicion de la constante llaves (llaves - valor) => (valor - llaves)
+const llavesReversed =  Object.keys(llaves).reduce((result, key) => {
+  const value = llaves[key];
+  result[value] = key;
+  return result;
+}, {});
 
-  // nuevoTexto = texto.replace(/ai/g, "a");
-  // nuevoTexto = nuevoTexto.replace(/enter/g, "e");
-  // nuevoTexto = nuevoTexto.replace(/imes/g, "i");
-  // nuevoTexto = nuevoTexto.replace(/ober/g, "o");
-  // nuevoTexto = nuevoTexto.replace(/ufat/g, "u");
-
-  nuevoTexto = texto.replace(/enter/g, "e");
-  nuevoTexto = nuevoTexto.replace(/imes/g, "i");
-  nuevoTexto = nuevoTexto.replace(/ai/g, "a");
-  nuevoTexto = nuevoTexto.replace(/ober/g, "o");
-  nuevoTexto = nuevoTexto.replace(/ufat/g, "u");
-  document.querySelector("#encriptado").innerHTML = nuevoTexto;
+function encriptarTexto (texto, llaves){
+  let newpalabra = texto.value;
+  Object.keys(llaves).forEach((element) => {
+    let expReg = new RegExp(element, 'g');
+    newpalabra = newpalabra.replace(expReg, llaves[element]);
+  })
+  return newpalabra;
 }
 
-function copiar() {}
+function verifyString(texto){
+  let invalid = 0;
+  let newData = [...texto.value];
+  for(i = 0; i < newData.length; i++){
+    let code = newData[i].charCodeAt(0);
+    if ( !(code >= 92 && code <= 122 || code === 241) ) {
+      invalid++;
+      break;
+    }
+  }
+  return invalid;
+}
+
+btnDesencriptar.addEventListener('click', () => {
+  if ( verifyString(inputText) !== 0 ){
+    alert("Solo se permiten Minusculas");
+    inputText.value = "";
+  } else {
+    if(outpuMsg.classList.contains('output_none')){
+      outpuMsg.classList.toggle('output_none');
+      outputInicial.classList.toggle('output_none');
+    }
+    output.innerHTML = encriptarTexto(inputText, llaves);
+    inputText.value = "";
+  }
+})
+
+btnEncriptar.addEventListener('click', () => {
+  if ( verifyString(inputText) !== 0 ){
+    alert("Solo se permiten Minusculas");
+    inputText.value = "";
+  } else {
+    if(outpuMsg.classList.contains('output_none')){
+      outpuMsg.classList.toggle('output_none');
+      outputInicial.classList.toggle('output_none');
+    }
+    output.innerHTML = encriptarTexto(inputText, llaves);
+    inputText.value = "";
+  }
+})
